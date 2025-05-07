39ef60bf010a24667cba9a5ca0392640de3307d3
Basti <sebastian.krol@outlook.com>
2025-05-07 09:05:48 +0200

    fix: update TypeScript configuration and add proper React types

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="react-scripts" />
+
+declare module '*.svg' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg';
+declare module '*.png';
+declare module '*.mp3';
+declare module '*.wav';
+
+declare module 'react' {
+  interface FunctionComponent<P = {}> {
+    (props: PropsWithChildren<P>, context?: any): ReactElement<any, any> | null;
+  }
+}
