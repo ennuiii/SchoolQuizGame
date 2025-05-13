e9a5e5381e98dad048e9c1c2adf085b54e3f7c6d
Krol <s.krol@gopus.de>
2025-05-13 14:03:06 +0200

    fix: Add explicit /spectator route to App for direct navigation and role switching

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ const App: React.FC = () => {
               ? <Spectator />
               : <Player />
           } />
+          <Route path="/spectator" element={<Spectator />} />
         </Routes>
       </Container>
     </Router>
