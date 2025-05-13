d4f46428d86e4dd730c550eb11f863fdf3641b73
Basti <sebastian.krol@outlook.com>
2025-05-13 17:48:13 +0200

    chore: consolidate all styles into App.css and remove redundant CSS files

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import './styles/shared-components.css';
+import './App.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
